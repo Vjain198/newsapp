208d909166d2242fe8ad78785ab710a55e988c5b
vikash jain <vikash.jain@lybrate.com>
2023-07-21 17:53:38 +0530

    css changes

--- src/compoents/Search/index.js
+++ src/compoents/Search/index.js
@@ -1,26 +1,27 @@
-import { useState } from 'react'
-import { useGlobalContext } from '../../context/context'
+import { useState } from "react";
+import { useGlobalContext } from "../../context/context";
 
 const Search = () => {
-  const [value, setValue] = useState('')
+  const [value, setValue] = useState("");
   const searchHandler = (e) => {
-    setValue(e.target.value)
-    searchPost(e.target.value)
-  }
-  const { searchPost } = useGlobalContext()
+    setValue(e.target.value);
+    searchPost(e.target.value);
+  };
+  const { searchPost } = useGlobalContext();
   return (
     <>
       <h1> 🔍 Search Your Favourite Topic News Blog</h1>
       <form onSubmit={(e) => e.preventDefault()}>
         <input
-          type='text'
-          placeholder='Enter your query.....'
+          type="text"
+          placeholder="Enter your query....."
           value={value}
           onChange={(e) => searchHandler(e)}
+          style={{ border: "1px solid #e6e6e6", borderRadius: "5px" }}
         />
       </form>
     </>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
--- src/compoents/Stories/index.js
+++ src/compoents/Stories/index.js
@@ -1,36 +1,38 @@
-import { useGlobalContext } from '../../context/context'
+import { useGlobalContext } from "../../context/context";
 
 const Stories = () => {
-  const { hits, isLoading, removePost } = useGlobalContext()
+  const { hits, isLoading, removePost } = useGlobalContext();
 
   if (isLoading) {
-    return <h1>Data is fetching.....</h1>
+    return <h1>Data is fetching.....</h1>;
   }
 
   return (
     <>
-      <div className='stories-div'>
-        {hits.map((currPost) => {
-          const { title, author, objectID, url, num_comments } = currPost
-          return (
-            <div key={objectID} className='card'>
-              <h2>{title}</h2>
-              <p>
-                ✍️&nbsp; By <span>{author}</span> &nbsp;|&nbsp; 💬 &nbsp;
-                <span>{num_comments}</span> comments
-              </p>
-              <div className='card-button'>
-                <a href={url}>Read more</a>
-                <a href='#' onClick={() => removePost(objectID)}>
-                  Remove
-                </a>
+      <div className="stories-div">
+        <div className="stories-div-items">
+          {hits.map((currPost) => {
+            const { title, author, objectID, url, num_comments } = currPost;
+            return (
+              <div key={objectID} className="card">
+                <h2>{title}</h2>
+                <p>
+                  ✍️&nbsp; By <span>{author}</span> &nbsp;|&nbsp; 💬 &nbsp;
+                  <span>{num_comments}</span> comments
+                </p>
+                <div className="card-button">
+                  <a href={url}>Read more</a>
+                  <a href="#" onClick={() => removePost(objectID)}>
+                    Remove
+                  </a>
+                </div>
               </div>
-            </div>
-          )
-        })}
+            );
+          })}
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Stories
+export default Stories;
